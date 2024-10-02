6f32340d6568d3cd1e7f99035e805ea783a34ccf
Thamsanqa Dyantyi <Thamsanqa.Dyantyi@sita.co.za>
2024-10-02 11:50:35 +0200

    Initial commit with existing project files

---
+++ src/test/java/com/tutorial/FullstackTuorial/FullstackTuorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.FullstackTuorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTuorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
